2d3d7066c84785088f68f45b2eacc20ebf7ee21c
Aleksey Yudin <heylecs@gmail.com>
2024-03-23 14:56:57 +0300

    fix: readme.md, index.js

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Проектная работа Mesto
-
+https://github.com/Aleksey-yudin/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const places = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+initialCards.forEach(function(item) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__title').textContent = item.name;
 
-// @todo: Функция удаления карточки
+    places.append(cardElement);
+});
 
-// @todo: Вывести карточки на страницу
+const delButton = document.querySelectorAll('.card__delete-button');
+
+delButton.forEach(button => {
+  button.addEventListener('click', function(event) {
+    const cardItem = event.target.closest('.places__item');
+    if (cardItem) {
+      cardItem.remove();
+    }
+  });
+});
